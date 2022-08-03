82606dad5552a06888065d1eccacbf40fa2420be
Kevin <admin@macbook-pro.mynetworksettings.com>
2022-08-03 17:18:57 -0400

    skeleton of home page without styling

--- client/components/Home.js
+++ client/components/Home.js
@@ -8,8 +8,18 @@ export const Home = (props) => {
   // const { username } = props;
 
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="row">
+      <div className="col">
+        <h1 className="text-center">Orca Audio</h1>
+        <div className="row">
+          <h2>Category 1</h2>
+          <div className="col-md-3 col-sm-6 mb-5"></div>
+        </div>
+        <div class>
+          <h2>Category 2</h2>
+          <div className="col-md-3 col-sm-6 mb-5"></div>
+        </div>
+      </div>
     </div>
   );
 };
