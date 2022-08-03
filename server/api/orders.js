d5696cff5e8496a9974631d3543ced9b17108bcf
Connor Choi <choic8516@gmail.com>
2022-08-03 10:45:46 -0400

    create apis for orders/products/users

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,19 +1,22 @@
 const router = require("express").Router();
 const {
-  models: { User, Order },
+  models: { Order, Product, User },
 } = require("../db");
 module.exports = router;
 
 router.get("/", async (req, res, next) => {
   try {
-    const users = await User.findAll({
+    const orders = await Order.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ["id", "firstName", "lastName", "email"],
-      include: [{ model: Order, as: "orders" }],
+      attributes: ["id", "isOpen", "orderStatus"],
+      include: [
+        { model: Product, as: "products" },
+        { model: User, as: "user" },
+      ],
     });
-    res.json(users);
+    res.json(orders);
   } catch (err) {
     next(err);
   }
