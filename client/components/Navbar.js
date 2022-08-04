3809c0e1b1ee15ec17f8d9c2d234ac92b0eee122
Ty Kuper <tykuper@gmail.com>
2022-08-04 08:08:27 -0700

    update navbar classname, add productlist pages

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 import { logout } from '../store';
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
-  <nav class="navbar navbar-expand-lg bg-secondary">
-    <div class="container-fluid">
+  <nav className="navbar navbar-expand-lg bg-secondary">
+    <div className="container-fluid">
       <span>
-        <Link class="navbar-brand" to="/home">
+        <Link className="navbar-brand" to="/home">
           Orca Audio
         </Link>
       </span>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent"
@@ -20,29 +20,29 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <Link class="nav-link active" aria-current="page" to="/products">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <Link className="nav-link active" aria-current="page" to="/products">
             Products
           </Link>
         </ul>
-        <div class="d-flex">
-          <Link class="nav-link active" aria-current="page" to="/user">
+        <div className="d-flex">
+          <Link className="nav-link active" aria-current="page" to="/user">
             User
           </Link>
-          <Link class="nav-link active" aria-current="page" to="/cart">
+          <Link className="nav-link active" aria-current="page" to="/cart">
             Cart
           </Link>
-          <form class="d-flex" role="search">
+          <form className="d-flex" role="search">
             <input
-              class="form-control me-2"
+              className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
             />
-            <button class="btn btn-outline-dark" type="submit">
+            <button className="btn btn-outline-dark" type="submit">
               Search
             </button>
           </form>
---
+++ client/store/products.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+
+const fetchProducts = (products) => ({
+  type: FETCH_PRODUCTS,
+  products,
+});
+
+export const fetchProductsThunk = () => async (dispatch) => {
+  try {
+    const { data: products } = await axios.get(`/api/products`);
+    dispatch(fetchProducts(products));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case FETCH_PRODUCTS:
+      return action.products;
+    default:
+      return state;
+  }
+}
