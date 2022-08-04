7935e615cd190f5471fec004468faab7225e5f62
Kevin <admin@macbook-pro.mynetworksettings.com>
2022-08-03 23:36:07 -0400

    made order confirmation component into a view

--- client/Routes.js
+++ client/Routes.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { withRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Login, Signup } from "./components/AuthForm";
 import Home from "./components/Home";
-import { OrderConfirmation } from "./components/OrderConfirmation";
+import { ConfirmationPageView } from "./views/confirmationPageView";
 import { me } from "./store";
 import SingleProductView from "./views/SingleProductView";
 
@@ -35,7 +35,7 @@ class Routes extends Component {
         <Switch>
           <Route path="/home" component={Home} />
           <Route path="/products/:productId" component={SingleProductView} />
-          <Route path="/confirmed/:orderId" component={OrderConfirmation} />
+          <Route path="/confirmed/:orderId" component={ConfirmationPageView} />
           <Redirect to="/home" />
         </Switch>
       </div>
