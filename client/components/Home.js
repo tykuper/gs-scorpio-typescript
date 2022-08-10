6092baacbdbab8f1728e98aa1794bf0846d9ca54
Kevin <admin@kevins-mbp.mynetworksettings.com>
2022-08-09 23:59:53 -0400

    added best sellers carousel

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { setCart } from "../store/cart";
+import { Carousel } from "react-bootstrap";
 
 /**
  * COMPONENT
@@ -54,13 +55,113 @@ export const Home = (props) => {
     fetchData().catch(console.error);
   }, [loggedInUser]);
 
+  // get filtered products for carousel
+  const [bestSellers, setBestSellers] = useState([]);
+  const [noiseCancelling, setNoiseCancelling] = useState([]);
+  const [lowStock, setLowStock] = useState([]);
+
+  useEffect(() => {
+    const fetchFiltered = async () => {
+      const { data: newBestSellers } = await axios.get(
+        "/api/products/category/best-seller"
+      );
+      const { data: newNoiseCancelling } = await axios.get(
+        "/api/products/category/noise-cancelling"
+      );
+      const { data: newLowStock } = await axios.get(
+        "/api/products/category/low-stock"
+      );
+
+      setBestSellers(newBestSellers);
+      setNoiseCancelling(newNoiseCancelling);
+      setLowStock(newLowStock);
+    };
+
+    fetchFiltered().catch(console.error);
+  }, []);
+
+  console.log(bestSellers);
+
   return (
     <div className="row">
       <div className="col">
         <h1 className="text-center">Orca Audio</h1>
         <div className="row">
-          <h2>Category 1</h2>
-          <div className="col-md-3 col-sm-6 mb-5"></div>
+          <h2>Check out our best sellers!</h2>
+          <Carousel variant="dark">
+            <Carousel.Item>
+              <img
+                className="d-block"
+                style={{
+                  margin: "auto",
+                  height: "300px",
+                  width: "600px",
+                }}
+                src={bestSellers[0]?.imageURL}
+                alt="First slide"
+              />
+              <Carousel.Caption
+                style={{ position: "relative", left: 0, top: 0 }}
+              >
+                <h4>{bestSellers[0]?.name}</h4>
+                <p>${bestSellers[0]?.price}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block"
+                style={{
+                  margin: "auto",
+                  height: "300px",
+                  width: "600px",
+                }}
+                src={bestSellers[1]?.imageURL}
+                alt="Second slide"
+              />
+              <Carousel.Caption
+                style={{ position: "relative", left: 0, top: 0 }}
+              >
+                <h4>{bestSellers[1]?.name}</h4>
+                <p>${bestSellers[1]?.price}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block"
+                style={{
+                  margin: "auto",
+                  height: "300px",
+                  width: "600px",
+                }}
+                src={bestSellers[2]?.imageURL}
+                alt="Third slide"
+              />
+              <Carousel.Caption
+                style={{ position: "relative", left: 0, top: 0 }}
+              >
+                <h4>{bestSellers[2]?.name}</h4>
+                <p>${bestSellers[2]?.price}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block"
+                style={{
+                  margin: "auto",
+                  height: "300px",
+                  width: "600px",
+                }}
+                src={bestSellers[3]?.imageURL}
+                alt="Third slide"
+              />
+              <Carousel.Caption
+                style={{ position: "relative", left: 0, top: 0 }}
+              >
+                <h4>{bestSellers[3]?.name}</h4>
+                <p>${bestSellers[3]?.price}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </div>
         <div className="row">
           <h2>Category 2</h2>
@@ -78,6 +179,7 @@ const mapState = (state) => {
   return {
     loggedInUser: state.auth,
     cart: state.cart,
+    isAdmin: state.auth.isAdmin !== undefined ? state.auth.isAdmin : false,
   };
 };
 
